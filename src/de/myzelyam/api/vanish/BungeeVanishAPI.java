f82c2d0904ab262c8d0a17684ef433af09c6abf0
MyzelYam <MyzelYam@users.noreply.github.com>
2016-08-07 19:04:49 +0200

    Create BungeeVanishAPI.java

---
+++ src/de/myzelyam/api/vanish/BungeeVanishAPI.java
@@ -0,0 +1,61 @@
+package de.myzelyam.api.vanish;
+
+import de.myzelyam.premiumvanish.bungeecord.PremiumVanishProxy;
+import de.myzelyam.premiumvanish.main.utilities.Validation;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * API for PremiumVanish on BungeeCord
+ * Method content is redacted
+ * */
+public class BungeeVanishAPI {
+
+    /**
+     * @return A list of the UUIDs of all online hidden players
+     */
+    public static List<UUID> getInvisiblePlayers() {
+        // ...
+        return null;
+    }
+
+    /**
+     * @return A list of the UUIDs of all hidden players, online or offline
+     * <p/>
+     * Deprecated: Will cause minor lag, use asynchronously or sparingly
+     */
+    @Deprecated
+    public static List<UUID> getAllInvisiblePlayers() {
+        // ...
+        return null;
+    }
+
+    /**
+     * @param p - the player.
+     * @return TRUE if the player is invisible and online, FALSE otherwise.
+     */
+    public static boolean isInvisible(ProxiedPlayer p) {
+        // ...
+        return false;
+    }
+
+    /**
+     * Makes the player's server hide the player
+     *
+     * @param p - the player.
+     */
+    public static void hidePlayer(ProxiedPlayer p) {
+        // ...
+    }
+
+    /**
+     * Makes the player's server show the player
+     *
+     * @param p - the player.
+     */
+    public static void showPlayer(ProxiedPlayer p) {
+        // ...
+    }
+}
